f3d1e00f82b92c0573ffd4645e49f4056ffd5aee
masum <masummir773@gmail.com>
2026-06-10 20:44:01 +0600

    wihtout authentication login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { Toaster }            from 'react-hot-toast'
-import { AppLayout }          from './components/layout/AppLayout'
-import { ProtectedRoute }     from './components/layout/ProtectedRoute'
-import LoginPage              from './pages/LoginPage'
-import HomePage               from './pages/HomePage' 
-import UploadPage             from './pages/UploadPage'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { AppLayout } from "./components/layout/AppLayout";
+import { ProtectedRoute } from "./components/layout/ProtectedRoute";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import UploadPage from "./pages/UploadPage";
 
 function Protected({ children, roles }) {
   return (
     <ProtectedRoute allowedRoles={roles}>
       <AppLayout>{children}</AppLayout>
     </ProtectedRoute>
-  )
+  );
 }
 
 export default function App() {
@@ -19,20 +19,52 @@ export default function App() {
     <>
       <Toaster
         position="top-right"
-        toastOptions={{ style: { fontSize: '13px' } }}
+        toastOptions={{ style: { fontSize: "13px" } }}
       />
-      <Routes> 
+      <Routes>
         {/* Public */}
-        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+
+        {/*  test purpose */}
+        <Route
+          path="/"
+          element={
+            <AppLayout>
+              <HomePage />
+            </AppLayout>
+          }
+        />
+
+        <Route
+          path="/upload"
+          element={
+            <AppLayout>
+              <UploadPage />
+            </AppLayout>
+          }
+        />
 
         {/* Protected */}
-        <Route path="/"  element={<Protected><HomePage /></Protected>} />
-      
-        <Route path="/upload"    element={<Protected><UploadPage /></Protected>} />
- 
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route
+          path="/"
+          element={
+            <Protected>
+              <HomePage />
+            </Protected>
+          }
+        />
 
+        <Route
+          path="/upload"
+          element={
+            <Protected>
+              <UploadPage />
+            </Protected>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
-  )
+  );
 }
